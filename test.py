2eca1e0eabf0afa5831ab3827117070411ebac87
Ilya <ballukrainian@gmail.com>
2023-09-26 08:29:51 +0300

    deleted database file

--- test.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime, timedelta
-
-
-data = [(1234, 'Колбаса', '27.09.23'), (21163, 'Cыр Радомер', '26.09.23')]
-
-
-sorted_data = sorted(data, key=lambda x: datetime.strptime(x[2], "%d.%m.%y"))
-
-print(sorted_data)
